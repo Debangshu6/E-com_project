318b90f71c3d5b0f81d9dc36c7817bec2a081779
Debangshu6 <gopal6291441556@gmail.com>
2025-07-08 10:55:10 +0530

    Add files via upload

---
+++ EComSpringApplication.java
@@ -0,0 +1,13 @@
+package codex_rishi.ecom_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EComSpringApplication.class, args);
+    }
+
+}
---
+++ EComSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package codex_rishi.ecom_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
